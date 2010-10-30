2922d07f7757c9106b4e4bc1652c61c96b2beb7c
Adam Weller <minch@trazzler.com>
2010-10-30 16:05:34 -0400

    My vimrc tab settings were whacked.

--- lib/constantize.rb
+++ lib/constantize.rb
@@ -2,34 +2,34 @@ module Constantize
   def self.included(base)  
     base.extend ClassMethods  
   end  
-	
-	module ClassMethods
-		extend ActiveSupport::Memoizable
 
-		def constantize(field)
-			@field = field # Yes, that's an instance variable at the class level
-		end
+  module ClassMethods
+    extend ActiveSupport::Memoizable
 
-		def const_missing(*args)
-			if args.size == 1
-				value = args[0].to_s.downcase
-				value = constant_id_for(@field, value)
-				return value if value
-			end
+    def constantize(field)
+      @field = field # Yes, that's an instance variable at the class level
+    end
 
-			super(*args)
-		end
+    def const_missing(*args)
+      if args.size == 1
+        value = args[0].to_s.downcase
+        value = constant_id_for(@field, value)
+        return value if value
+      end
 
-		def constant_id_for(field, value)
-			method = "find_by_#{field}"
+      super(*args)
+    end
 
-			value = self.send(method, value) rescue nil
-			value.id if value
-		end
-		memoize :constant_id_for
-	end
+    def constant_id_for(field, value)
+      method = "find_by_#{field}"
+
+      value = self.send(method, value) rescue nil
+      value.id if value
+    end
+    memoize :constant_id_for
+  end
 end
 
 class ActiveRecord::Base
-	include Constantize
+  include Constantize
 end
